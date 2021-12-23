f2bbc19c2258158c210371d7be3d4cc62e15121e
jsotobor <jsotobor@SVQ-1WCF9C3.usersad.everis.int>
2021-12-23 12:24:20 +0100

    correccion funcional

--- src/main/java/funcional/controller/controller.java
+++ src/main/java/funcional/controller/controller.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import funcional.persona.persona;
@@ -17,7 +18,7 @@ public class controller {
 			);
 	
 	@GetMapping("/login")
-	public String login(String usuario, String password) {
+	public String login(@RequestParam(value = "usuario") String usuario,@RequestParam(value = "password") String password) {
 		List<persona> user = listUsuarios.stream().filter(usur -> (usur.getUsuario().equals(usuario)) && (usur.getPassword().equals(password))).collect(Collectors.toList());
 		return "Hola " + user.get(1).getNombre();
 	}
